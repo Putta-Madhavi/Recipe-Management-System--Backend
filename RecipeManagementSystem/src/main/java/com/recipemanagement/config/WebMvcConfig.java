fd1aa5b004689053ccd70438d3156ddaf4eea1b9
Putta Madhavi <madhaviputta44@gmail.com>
2025-07-04 12:35:32 +0530

    Update WebMvcConfig.java

--- RecipeManagementSystem/src/main/java/com/recipemanagement/config/WebMvcConfig.java
+++ RecipeManagementSystem/src/main/java/com/recipemanagement/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") 
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500") 
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:7090") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
                 .allowedHeaders("*") 
                 .allowCredentials(true) 
